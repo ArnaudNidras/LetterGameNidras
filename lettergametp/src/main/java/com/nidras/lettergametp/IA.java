eab7cf926d935dece99d5474769f4eb9f9dd7bcb
Franck Valot <franckvalot@gmail.com>
2017-02-27 23:26:16 +0100

    IA stealing words added

--- lettergametp/src/main/java/com/nidras/lettergametp/IA.java
+++ lettergametp/src/main/java/com/nidras/lettergametp/IA.java
@@ -56,7 +56,71 @@ public class IA extends Player{
 		playableWords = game.getDictionary().wordMaker(poolContent);
 		
 		if(playableWords.size() > 0 && hasPlayed) play();
+
+
+		else {
+			for(int j = 0 ; j < game.getPlayerPool().getNumberOfElements() ; j ++) {
+				
+				poolContent = game.getPlayerPool().getElement(j).toString();
+				
+				for(int i = 0 ; i < game.getCommonPool().getNumberOfElements() ; i ++) poolContent += game.getCommonPool().getElement(i);
+				
+				System.out.println("Pool : " + poolContent);
+				
+				playableWords = game.getDictionary().wordMaker(poolContent);
+				
+				for(int i = 0 ; i < playableWords.size() ; i ++) {
+					System.out.println("Playable : " + playableWords.get(i));
+						if(!playableWords.get(i).contains(game.getPlayerPool().getElement(j).toString()) || playableWords.get(i).length() <= game.getPlayerPool().getElement(j).toString().length()) {
+						System.out.println("Deleting : " + playableWords.get(i));
+						playableWords.remove(i);
+						i --;
+						
+					}
+				}
+				toPlay = "";
+				for(String i : playableWords){
+					
+					if(game.getDictionary().countVowel(i) > game.getDictionary().countVowel(toPlay) && !game.getIAPool().containsWord(i)) toPlay = i;
+					System.out.println("Toplay : " + toPlay);
+					
+				}
+					
+				
+				if(toPlay.length() > 0) {
+
+					game.getIAPool().addElement(toPlay);
+					System.out.println("Played : " + toPlay);
+					toPlay = toPlay.replace(game.getPlayerPool().getElement(j).toString(), "");
+					System.out.println("Removing : " + toPlay);
+					
+					for(int i = 0 ; i < toPlay.length() ; i ++) game.getCommonPool().removeElement(toPlay.charAt(i));
+					
+					game.getPlayerPool().removeElement(game.getPlayerPool().getElement(j).toString());
+					j--;
+					game.getCommonPool().addElement(game.getPlayer().drawLetter());
+					game.getGUI().update();
+					hasPlayed = true;
+					try {
+						
+						Thread.sleep(1000);
+						
+					} catch (InterruptedException e) {
+						
+						e.printStackTrace();
+					}
+				}				
+				
+			}	
+			
+		}
+		poolContent = "";
 		
+		for(int i = 0 ; i < game.getCommonPool().getNumberOfElements() ; i ++) poolContent += game.getCommonPool().getElement(i);
+		
+		playableWords = game.getDictionary().wordMaker(poolContent);
+		
+		if(playableWords.size() > 0 && hasPlayed) play();
 		
 	}
 
